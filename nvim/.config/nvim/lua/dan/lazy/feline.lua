333e7e736783b8eab99f29daee81972f76485a6e
StateOfDenial <daniel.brown715@gmail.com>
2024-05-11 19:25:14 +0800

    customize feline nvim bar colours also stop auto installing terraformls

--- nvim/.config/nvim/lua/dan/lazy/feline.lua
+++ nvim/.config/nvim/lua/dan/lazy/feline.lua
@@ -4,7 +4,20 @@ return {
     "lewis6991/gitsigns.nvim",
   },
   config = function()
-    require('feline').setup()
+    local everforest = {
+        bg = "#2D353B",
+        fg = "#D3C6AA",
+        red = "#E67E80",
+        green = "#a7c080",
+        skyblue = "#7FBBB3",
+        cyan = "#83C092",
+        black = "#232A2E",
+        magenta = "#D699B6",
+        orange = "#E69875",
+        yellow = "#DBBC7F",
+    }
+    require('feline').add_theme('everforest', everforest)
+    require('feline').setup({theme = 'everforest'})
     require('feline').winbar.setup()
   end
 }
