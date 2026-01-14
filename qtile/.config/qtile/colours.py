5776dcdcf6ea4b7903d198e650ac9e19ac599f40
Daniel Brown <stubborndenial@proton.me>
2026-01-14 10:52:30 +0800

    forgot colours qtile file

---
+++ qtile/.config/qtile/colours.py
@@ -0,0 +1,12 @@
+everforest = {
+    "background": ["#2E383C", "#2E383C"],  # BG
+    "black": ["#282828", "#282828"],  # More black?
+    "foreground": ["#D3C6AA", "#D3C6AA"],  # FG
+    "aqua": ["#83C092", "#83C092"],  # Aqua
+    "green": ["#A7C080", "#A7C080"],  # Green
+    "yellow": ["#DBBC7F", "#DBBC7F"],  # Yellow
+    "blue": ["#7FBBB3", "#7FBBB3"],  # Blue
+    "purple": ["#D699B6", "#D699B6"],  # Purple
+    "red": ["#E69875", "#E69875"],  # Red
+    "status_line_3": ["#E67E80", "#E67E80"]  # Status line 3
+}
